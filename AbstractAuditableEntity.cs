f03f8073eeb0b24b7f2e90db2c3da6a9cc9b0db3
jpg-consulting <opensource@jpg-consulting.es>
2017-12-25 14:17:14 +0100

    New entities

---
+++ AbstractAuditableEntity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceNet.Data.Entities
+{
+    public abstract class AbstractAuditableEntity
+    {
+        private String m_createdBy;
+        private DateTime m_createdAt;
+        private String m_updatedBy;
+        private DateTime m_updatedAt;
+
+        public String CreatedBy
+        {
+            get { return m_createdBy; }
+        }
+
+        public DateTime CreatedAt
+        {
+            get { return m_createdAt; }
+        }
+
+        public String UpdatedBy
+        {
+            get { return m_updatedBy; }
+        }
+
+        public DateTime UpdatedAt
+        {
+            get { return m_updatedAt; }
+        }
+    }
+}
---
+++ GenericUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceNet.Data.Entities
+{
+    class GenericUser : AbstractAuditableEntity
+    {
+        private UInt32 m_userId;
+        private String m_userName;
+
+        public UInt32 UserId
+        {
+            get { return m_userId; }
+        }
+
+        public String UserName
+        {
+            get { return m_userName; }
+            set { m_userName = value; }
+        }
+    }
+}
