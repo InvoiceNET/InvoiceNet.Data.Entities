238155c2e9748212d925c3acc014dd25e7fc7cc3
jpg-consulting <opensource@jpg-consulting.es>
2017-12-25 14:05:25 +0100

    first commit

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceNet.Data.Entities
+{
+    public class Class1
+    {
+    }
+}
